5657d4d69003123d4b73a1a515ca163e1cf6d0fa
Thiago Vieira <pessoalvieira779@gmail.com>
2026-03-29 18:55:43 -0300

    feat: daily report

--- data.js
+++ data.js
@@ -1,98 +1,105 @@
 const projectsByDate = {
-	'2026-03-14': [
-		{
-			title: 'To Do List',
-			link: 'March/todo-list-project/index.html',
-		},
-	],
-	'2026-03-15': [
-		{
-			title: 'To Do List (improvements)',
-			link: 'March/todo-list-project/index.html',
-		},
-	],
-	'2026-03-16': [
-		{
-			title: 'To Do List (tests)',
-			link: 'March/todo-list-project/index.html',
-		},
-	],
-	'2026-03-17': [
-		{
-			title: 'To Do List (goals system / mobile improvements)',
-			link: 'March/todo-list-project/index.html',
-		},
-	],
-	'2026-03-18': [
-		{
-			title: 'To Do List (Custom goals / colors / bugs)',
-			link: 'March/todo-list-project/index.html',
-		},
-	],
-	'2026-03-18': [
-		{
-			title: 'To Do List (Task sorting)',
-			link: 'March/todo-list-project/index.html',
-		},
-	],
-	'2026-03-19': [
-		{
-			title: 'To Do List (UI improvements / motivational quotes / streak system)',
-			link: 'March/todo-list-project/index.html',
-		},
-	],
-	'2026-03-20': [
-		{
-			title: 'Novel Day (Project planning / concept)',
-			link: 'March/novel-day/index.html',
-		},
-	],
-	'2026-03-21': [
-		{
-			title: 'To Do List (Periodic habits / erasing finished tasks)',
-			link: 'March/todo-list-project/index.html',
-		},
-	],
-	'2026-03-22': [
-		{
-			title: 'Novel Day (Basic elements / Initial files)',
-			link: 'March/novel-day/index.html',
-		},
-	],
-	'2026-03-23': [
-		{
-			title: 'Novel Day (Studying other writing apps / UI improvements)',
-			link: 'March/novel-day/index.html',
-		},
-	],
-	'2026-03-24': [
-		{
-			title: 'Novel Day (Style improvement / Initial script)',
-			link: 'March/novel-day/index.html',
-		},
-	],
-	'2026-03-25': [
-		{
-			title: 'Novel Day (Editable content / Text selection)',
-			link: 'March/novel-day/index.html',
-		},
-	],
-	'2026-03-26': [
-		{
-			title: 'Novel Day (Novel content style)',
-			link: 'March/novel-day/index.html',
-		},
-	],
-	'2026-03-27': [
-		{
-			title: 'Novel Day (Temporary text modifier buttons)',
-			link: 'March/novel-day/index.html',
-		},
-	],
-	'2026-03-28': [
-		{
-			title: 'Novel Day (Text modifier buttons improvements)',
-			link: 'March/novel-day/index.html',
-		},
-	],
+  "2026-03-14": [
+    {
+      title: "To Do List",
+      link: "March/todo-list-project/index.html",
+    },
+  ],
+  "2026-03-15": [
+    {
+      title: "To Do List (improvements)",
+      link: "March/todo-list-project/index.html",
+    },
+  ],
+  "2026-03-16": [
+    {
+      title: "To Do List (tests)",
+      link: "March/todo-list-project/index.html",
+    },
+  ],
+  "2026-03-17": [
+    {
+      title: "To Do List (goals system / mobile improvements)",
+      link: "March/todo-list-project/index.html",
+    },
+  ],
+  "2026-03-18": [
+    {
+      title: "To Do List (Custom goals / colors / bugs)",
+      link: "March/todo-list-project/index.html",
+    },
+  ],
+  "2026-03-18": [
+    {
+      title: "To Do List (Task sorting)",
+      link: "March/todo-list-project/index.html",
+    },
+  ],
+  "2026-03-19": [
+    {
+      title:
+        "To Do List (UI improvements / motivational quotes / streak system)",
+      link: "March/todo-list-project/index.html",
+    },
+  ],
+  "2026-03-20": [
+    {
+      title: "Novel Day (Project planning / concept)",
+      link: "March/novel-day/index.html",
+    },
+  ],
+  "2026-03-21": [
+    {
+      title: "To Do List (Periodic habits / erasing finished tasks)",
+      link: "March/todo-list-project/index.html",
+    },
+  ],
+  "2026-03-22": [
+    {
+      title: "Novel Day (Basic elements / Initial files)",
+      link: "March/novel-day/index.html",
+    },
+  ],
+  "2026-03-23": [
+    {
+      title: "Novel Day (Studying other writing apps / UI improvements)",
+      link: "March/novel-day/index.html",
+    },
+  ],
+  "2026-03-24": [
+    {
+      title: "Novel Day (Style improvement / Initial script)",
+      link: "March/novel-day/index.html",
+    },
+  ],
+  "2026-03-25": [
+    {
+      title: "Novel Day (Editable content / Text selection)",
+      link: "March/novel-day/index.html",
+    },
+  ],
+  "2026-03-26": [
+    {
+      title: "Novel Day (Novel content style)",
+      link: "March/novel-day/index.html",
+    },
+  ],
+  "2026-03-27": [
+    {
+      title: "Novel Day (Temporary text modifier buttons)",
+      link: "March/novel-day/index.html",
+    },
+  ],
+  "2026-03-28": [
+    {
+      title: "Novel Day (Text modifier buttons improvements)",
+      link: "March/novel-day/index.html",
+    },
+  ],
+  "2026-03-29": [
+    {
+      title: "Novel Day (Unwrapping text / Bug fixes)",
+      link: "March/novel-day/index.html",
+    },
+  ],
 };
